4801d3038301f8d9a934784863fd29fd77737108
HYUNJUN YOOK <yhj03069@gmail.com>
2023-01-10 14:44:11 +0900

    주석처리

--- 20221208 palindrome.py
+++ 20221208 palindrome.py
@@ -1,22 +1,22 @@
-# 재귀함수로 풀기
-def checker(space, count):
-    def palindrome(count, left, right):
-        while left < right:
-            if space[left] != space[right]:
-                if count == 0:
-                    return False
-                else:
-                    return palindrome(count - 1, left + 1, right) or palindrome(count - 1, left, right - 1)
-            left += 1
-            right -= 1
-        return True
-
-    return palindrome(count, 0, len(space) - 1)
-
-
-space = []
-count = 0
-space, count = input().split()
-space = list(space)
-
-print(checker(space, int(count)))
+# # 재귀함수로 풀기
+# def checker(space, count):
+#     def palindrome(count, left, right):
+#         while left < right:
+#             if space[left] != space[right]:
+#                 if count == 0:
+#                     return False
+#                 else:
+#                     return palindrome(count - 1, left + 1, right) or palindrome(count - 1, left, right - 1)
+#             left += 1
+#             right -= 1
+#         return True
+#
+#     return palindrome(count, 0, len(space) - 1)
+#
+#
+# space = []
+# count = 0
+# space, count = input().split()
+# space = list(space)
+#
+# print(checker(space, int(count)))
