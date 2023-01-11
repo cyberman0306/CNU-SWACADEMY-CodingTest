0c598f14bab4422e3ecdd961e56207776c20f37d
HYUNJUN YOOK <yhj03069@gmail.com>
2023-01-11 11:07:18 +0900

    주석처리

--- 230111 중요한것은 꺾이지않는 마음.py
+++ 230111 중요한것은 꺾이지않는 마음.py
@@ -2,21 +2,16 @@ def seller(space, incomeTarget, buyingDate):
     space2 = []
     for i in space[int(buyingDate) - 1: ]:
         space2.append(i)
-    #print(space2)
     incomeTarget = int(space[int(buyingDate)-1]) + incomeTarget
-    #print(incomeTarget)
-    #print(space2.index(str(incomeTarget)))
-    if str(incomeTarget) in space2:
-        answer = space2.index(str(incomeTarget))
-        answer += buyingDate
-        return answer
-    else:
-        return "JB"
-
+    for j in range(len(space2)):
+        if int(space2[j]) >= incomeTarget:
+            answer = j + buyingDate
+            break
+        else:
+            answer = "JB"
+    return answer
 
 n = int(input())
-
-
 
 for i in range(n):
     n, incomeTarget, buyingDate = map(int, input().split())
@@ -25,5 +20,3 @@ for i in range(n):
     space = list(space)
     #print(space)
     print(seller(space, incomeTarget, buyingDate))
-
-# 목표값 이상이기만 하면됨
