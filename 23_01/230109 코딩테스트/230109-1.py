6083de42b26faa7fdc597d725967c688a81d7c87
HYUNJUN YOOK <yhj03069@gmail.com>
2023-01-11 11:34:53 +0900

    완료

--- 23_01/230109 코딩테스트/230109-1.py
+++ 23_01/230109 코딩테스트/230109-1.py
@@ -1,32 +1,25 @@
 
-# import sys
-# input = sys.stdin.readline
-# import decimal
-#
-# context = decimal.getcontext()
-# context.rounding = decimal.ROUND_HALF_UP
-#
-# for _ in range(int(input())):
-#     n, u = input().split()
-#     if u == 'K':
-#         answer = float(n) / 1.6
-#         answer = round(decimal.Decimal(answer), 2)
-#         if str(answer)[-1] == '0':
-#             print(float(str(answer)[:-1]))
-#         else:
-#             print(answer)
-#     else:
-#         answer = float(n) * 1.6
-#         answer = round(decimal.Decimal(answer), 2)
-#         if str(answer)[-1] == '0':
-#             print(float(str(answer)[:-1]))
-#         else:
-#             print(answer)
+import sys
+input = sys.stdin.readline
+import decimal
 
-#
-# import decimal
-#
-# context = decimal.getcontext()
-# context.rounding = decimal.ROUND_HALF_UP
-#
-# print(round(decimal.Decimal('2.5'), 0))  # 3
+context = decimal.getcontext()
+context.rounding = decimal.ROUND_HALF_UP
+
+for _ in range(int(input())):
+    n, u = input().split()
+    if u == 'K':
+        answer = float(n) / 1.6
+        answer = round(decimal.Decimal(answer), 2)
+        if str(answer)[-1] == '0':
+            print(float(str(answer)[:-1]))
+        else:
+            print(answer)
+    else:
+        answer = float(n) * 1.6
+        answer = round(decimal.Decimal(answer), 2)
+        if str(answer)[-1] == '0':
+            print(float(str(answer)[:-1]))
+        else:
+            print(answer)
+
--- 23_01/실전문제/Umm문자열2/230110 Umm문자열 심화.py
+++ 23_01/실전문제/Umm문자열2/230110 Umm문자열 심화.py
@@ -1,25 +1,25 @@
-# def checker(space, answer):
-#     if len(space) >= 3 and space[0] == "U" and space.count("U") == 1:
-#         answer += 1
-#         return answer
-#     elif len(space) < 3 or "U" not in space or space.count("m") < 2:
-#         return answer
-#     else:
-#         space.remove(space[0])
-#         checker(space, answer)
-#     return answer
-#
-#
-# n = int(input())
-# for i in range(n):
-#     length, count = map(int, input().split())
-#     space = []
-#     space2 = []
-#     space = input()
-#     space = list(space)
-#     answer = 0
-#     for j in range(count):
-#         space2 = []
-#         a, b = map(int, input().split())
-#         answer = checker(space[a - 1:b], answer)
-#     print(answer)
+def checker(space, answer):
+    if len(space) >= 3 and space[0] == "U" and space.count("U") == 1:
+        answer += 1
+        return answer
+    elif len(space) < 3 or "U" not in space or space.count("m") < 2:
+        return answer
+    else:
+        space.remove(space[0])
+        checker(space, answer)
+    return answer
+
+
+n = int(input())
+for i in range(n):
+    length, count = map(int, input().split())
+    space = []
+    space2 = []
+    space = input()
+    space = list(space)
+    answer = 0
+    for j in range(count):
+        space2 = []
+        a, b = map(int, input().split())
+        answer = checker(space[a - 1:b], answer)
+    print(answer)
