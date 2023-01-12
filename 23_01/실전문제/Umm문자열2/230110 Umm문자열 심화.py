63345c85ad9c0501b2fdf200c0f393687f164e7a
HYUN JUN YOOK <yhj03069@gmail.com>
2023-01-12 10:03:09 +0900

    시간복잡도 수정중

--- 23_01/실전문제/Umm문자열2/230110 Umm문자열 심화.py
+++ 23_01/실전문제/Umm문자열2/230110 Umm문자열 심화.py
@@ -1,27 +1,33 @@
 def checker(space, answer):
-    if len(space) >= 3 and space[0] == "U" and space.count("U") == 1:
-        answer += 1
-        return answer
-    elif len(space) < 3 or "U" not in space or space.count("m") < 2:
-        return answer
-    else:
-        space.remove(space[0])
-        checker(space, answer)
+    if len(space) >= 3:
+        if space.count("U") == 1:
+            if space[0] == "U":
+                answer += 1
+                return answer
+            else:
+                checker(space[1:], answer)
     return answer
 
+def checker2(space, answer):
+    if space[0] == "U":
+        if len(space) >= 3:
+            if space.count("U") == 1:
+                answer += 1
+                return answer
+    else:
+        if "U" in space:
+            U = space.index("U")
+            checker2(space[U:], answer)
+    return answer
 
 n = int(input())
 for i in range(n):
     length, count = map(int, input().split())
-    space = []
-    space2 = []
     space = input()
     space = list(space)
     answer = 0
     for j in range(count):
         space2 = []
         a, b = map(int, input().split())
-        answer = checker(space[a - 1:b], answer)
+        answer = checker2(space[a - 1:b], answer)
     print(answer)
-
-
