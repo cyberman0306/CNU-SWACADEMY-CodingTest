6783cf35352733ac4391ad7c4960b3ea8e515f9d
yook <yhj03069@gmail.com>
2023-01-16 22:23:26 +0900

    완료

--- 23_01/230116/준식이의 모엄 1/준식이의 모엄 1.py
+++ 23_01/230116/준식이의 모엄 1/준식이의 모엄 1.py
@@ -6,25 +6,16 @@ for _ in range(t):
     for i in range(n):
         getSpace = input()
         getSpace = list(getSpace)
-        #print(getSpace)
         space.append(getSpace)
-    #print(space)
     countControl = int(input())
     control = input()
     control = list(control)
-    #print(countControl)
-    #print(control)
     totalspace = []
     for j in range(len(space)):
         for p in range(len(space[j])):
             totalspace.append(space[j][p])
-        #print(totalspace)
-    #print()
-    #print(totalspace.index("@"))
-    junsikRow, junsikColumn = (totalspace.index("@") % m, totalspace.index("@") // m)
-    print(totalspace.index("@") // m, totalspace.index("@") % m)
-    #print(totalspace.index("@") % m)
-    #print()
+
+    junsikRow, junsikColumn = (totalspace.index("@") // m + 1, totalspace.index("@") % m + 1)
 
     for i in range(countControl):
         if control[i] == "L":
@@ -36,21 +27,22 @@ for _ in range(t):
         elif control[i] == "D":
             junsikRow += 1
 
+        if junsikColumn < 1:
+            junsikColumn = 1
+        if junsikColumn > m:
+            junsikColumn = m
+        if junsikRow < 1:
+            junsikRow = 1
+        if junsikRow > n:
+            junsikRow = n
+        if space[junsikRow-1][junsikColumn-1] == "#":
+            if control[i] == "L":
+                junsikColumn += 1
+            elif control[i] == "R":
+                junsikColumn -= 1
+            elif control[i] == "U":
+                junsikRow += 1
+            elif control[i] == "D":
+                junsikRow -= 1
 
-        if junsikColumn <= 0:
-            junsikColumn = 0
-            print("wall")
-        if junsikColumn >= m:
-            junsikColumn = m - 1
-            print("wall")
-        if junsikRow <= 0:
-            print("wall")
-
-            junsikRow = 0
-        if junsikRow >= n:
-            print("wall")
-
-            junsikRow = n - 1
-        print(junsikRow, junsikColumn)
-
-    print("totoal", junsikRow, junsikColumn)
+    print(junsikRow, junsikColumn)
