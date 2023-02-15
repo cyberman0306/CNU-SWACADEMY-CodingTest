6016903f08b948936027d32f275c7092436c144c
yook <yhj03069@gmail.com>
2023-02-15 16:02:43 +0900

    부분점수맞음 수정필요

--- 23_02/230215/04/04.py
+++ 23_02/230215/04/04.py
@@ -2,22 +2,21 @@ n = int(input())
 liB = list(map(int, input().split()))
 m = int(input())
 liQ = list(map(int, input().split()))
+
 sumli = []
 sumit = 0
 ans = 0
 for i in range(len(liB)):
     sumit += liB[i]
     sumli.append(sumit)
-#print(sumli)
+
 for i in range(m):
     for j in range(len(sumli)):
-        #print(liQ[i], sumli[j])
         if liQ[i] <= sumli[j]:
-            #print(j+1)
             ans += j+1
-            #print()
+            break
+        if liQ[i] > sumli[-1]:
+            ans += n
             break
 
-#print()
-#print(ans)
 print(ans % 1000000007)
