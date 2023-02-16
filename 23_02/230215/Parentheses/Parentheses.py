b00f0ac1c7c03eb7bef7aa5f7f44078845b1bc25
HYUN JUN YOOK <yhj03069@gmail.com>
2023-02-16 10:28:07 +0900

    init, 완료

--- 23_02/230215/Parentheses/Parentheses.py
+++ 23_02/230215/Parentheses/Parentheses.py
@@ -1,12 +1,28 @@
 li = list(input())
-ans = "NO"
-if li.count('(') != li.count(')'):
-    ans = "NO"
-else:
-    for i in range(li.count('(')):
-        if li.index("(") < li.index(")"):
-            li.remove("(")
-            li.remove(")")
-    if len(li) == 0:
-        ans = "YES"
-print(ans)
+n = int(input())
+leftstack = [li[0]]
+li = li[1:]
+rightftstack = []
+for i in reversed(range(len(li))):
+    rightftstack.append(li[i])
+#print(rightftstack)
+
+for i in range(n):
+    order = input()
+    if order == "move right" and len(rightftstack) > 1:
+        leftstack.append(rightftstack[-1])
+        del rightftstack[-1]
+
+    elif order == "move left" and len(leftstack) > 1:
+        rightftstack.append(leftstack[-1])
+        del leftstack[-1]
+
+    elif order == "tear right" and len(rightftstack) > 1:
+        del rightftstack[-1]
+
+    elif order == "tear left" and len(leftstack) > 1:
+        del leftstack[-1]
+
+    #print(leftstack[-1], rightftstack[-1])
+
+print(leftstack[-1], rightftstack[-1])
